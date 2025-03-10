a3fdd299baa5eda83d6660809229b52e03fe82d6
yusuf-nuru-yesuf <yusufnuru2002@gmail.com>
2025-03-10 19:02:21 +0800

    feat: enhance server setup with CORS, cookie parsing, and improved logging

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,35 @@
 import 'dotenv/config';
-import express, { Express, Request, Response, NextFunction } from 'express';
-
-const app: Express = express();
-const port = process.env.PORT || 3000;
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import connectToDatabase from './config/db';
+import { NODE_ENV, APP_ORIGIN, PORT } from './constants/env';
+import errorHandler from './middleware/errorHandler';
+import authRoutes from './auth/authRoutes';
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cors({
+  origin: APP_ORIGIN,
+  credentials: true,
+}));
+app.use(cookieParser());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+app.use('/auth', authRoutes);
+
+app.use((req, res, next) => {
+  console.log('Request URL:', req.url);
+  console.log('Request originalUrl:', req.originalUrl);
+  console.log('Request baseUrl:', req.baseUrl);
+  console.log('Request path:', req.path);
+  console.log('Cookies:', req.cookies);
+  next();
 });
+app.use(errorHandler);
 
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+app.listen(PORT, async () => {
+  console.log(`[server]: Server is running at http://localhost:${PORT} in ${NODE_ENV} environment`);
+  await connectToDatabase();
 });
