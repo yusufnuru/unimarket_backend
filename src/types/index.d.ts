ef3a7b8eb4b20387a65b192008b2b321460372fd
yusuf-nuru-yesuf <yusufnuru2002@gmail.com>
2025-03-16 15:07:06 +0800

    feat(types): extend Express Request interface to include cookies with accessToken

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,6 +1,10 @@
 declare global {
   namespace Express {
     interface Request {
+      cookies: {
+        accessToken?: string;
+        [key: string]: string;
+      };
       userId: string;
       sessionId: string;
       role: string;
