905fac779d8a2b3f6d2d95e2b39b2e52ec4cbcb7
Yusuf-Nuru-Yesuf <yusufnuru2002@gmail.com>
2025-04-04 22:45:29 +0800

    fix(config): update output directory in drizzle configuration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  out: './src/drizzle/',
+  out: './drizzle/',
   schema: './src/schema/*.ts',
   dialect: 'postgresql',
   dbCredentials: {
